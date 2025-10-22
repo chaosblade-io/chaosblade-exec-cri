fc16e67d2ca469a37c9ec98878fe0f618a0712d9
Spencer Cai <spencercjh@gmail.com>
2025-10-22 10:03:14 +0800

    chore: introduce hawkeye to unify license header; update copyright to the ChaosBlade Authors (#31)
    Signed-off-by: spencercjh <spencercjh@gmail.com>

--- exec/container/cri-o/cri-o.go
+++ exec/container/cri-o/cri-o.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999-2020 Alibaba Group Holding Ltd.
+ * Copyright 2025 The ChaosBlade Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,4 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package cri_o
